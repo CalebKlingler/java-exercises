a4f1f5e98d14eb6f4e58e402541c8299d7e49bca
Caleb Klingler <cjkling6@gmail.com>
2019-08-01 18:54:09 -0500

    Added while loop functionality

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -10,6 +10,11 @@ public class Area {    public static void main(String[] args){
     in = new Scanner(System.in);
     System.out.println("Enter the radius of the circle: ");
     radius = in.nextDouble();
+    while (radius < 0){
+        System.out.println("No negative numbers allowed! Enter the radius of the circle: ");
+        radius= in.nextDouble();
+
+    }
 
     area = Math.PI * radius * radius;
     System.out.println("The area of the circle is " + area);
