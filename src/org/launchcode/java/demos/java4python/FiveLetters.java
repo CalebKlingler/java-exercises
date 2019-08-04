86960d773b77507c11816ee894fff8dd3a533644
Caleb Klingler <cjkling6@gmail.com>
2019-08-04 13:52:38 -0500

    Finished 5 letter word exercise

---
+++ src/org/launchcode/java/demos/java4python/FiveLetters.java
@@ -0,0 +1,24 @@
+package org.launchcode.java.demos.java4python;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FiveLetters {
+    public static void main(String[] args){
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("crazy", "apple", "cat","dog"));
+        int five_letter_words = fiveLetterWords(words);
+        System.out.println("There are " + five_letter_words + " five letter words in the list");
+
+    }
+    public static int fiveLetterWords(ArrayList<String> list){
+        int count = 0;
+        for (String word : list ){
+            if (word.length()==5){
+                count ++;
+            }
+
+        }
+        return count;
+
+    }
+}
